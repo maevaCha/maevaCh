7f1d48022ba13d5c3dd7e5d7e9260dcb6fd9f7eb
Maeva Charbonnier <mcharbonnier@mcharbonnier-vdi.vm.vdi.s1.p.fti.net>
2016-08-30 11:04:53 +0200

    menu only css in hover

--- portefolio/js/effect.js
+++ portefolio/js/effect.js
@@ -1,15 +1,3 @@
 $(document).ready(function(){
 
-	var menuHover = function(id) {
-		$(id).hover(	
-			function() {
-				$(id).removeClass('animation-down');
-				$(this).addClass('animation-up');
-			}, function() {
-				$(id).removeClass('animation-up');
-				$(this).addClass('animation-down');
-			}
-		);
-	};
-	menuHover($('.menu-emplacement'));
 });
