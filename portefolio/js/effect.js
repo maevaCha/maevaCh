a18389e28fed4d7af2ad68d7998a0af3fa410f5a
Maeva Charbonnier <mcharbonnier@mcharbonnier-vdi.vm.vdi.s1.p.fti.net>
2016-08-31 15:29:23 +0200

    click en menu

--- portefolio/js/effect.js
+++ portefolio/js/effect.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+	//click on menu
+	var menuClick = function(id) {
+		id.click(function() {
+			//click to see menu add class
+			$(this).toggleClass('active');
+    		$('.overlay').animate({width: 'toggle'}, 500);
+		
+		});
+	}
+	menuClick($('.menu-emplacement'));
 });
